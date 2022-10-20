a8bf9cffc2f02edbfed74212c93fa371b198d8bb
xan2301 <2silverghost2@gmail.com>
2022-10-20 13:05:07 +0200

    14. JPA Entities

---
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -0,0 +1,66 @@
+package guru.springframework.spring5webapp.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+
+//POJO
+
+@Entity
+
+public class Author {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+
+    private Long id;
+
+    private String firstName;
+    private String lastname;
+
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book>books;
+
+
+    public Author() {
+    }
+
+    public Author(String firstName, String lastname, Set<Book> books) {
+        this.firstName = firstName;
+        this.lastname = lastname;
+        this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+}
