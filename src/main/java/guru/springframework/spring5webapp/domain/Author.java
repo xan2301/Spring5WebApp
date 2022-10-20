6ad4115a08f950c6450333dccdbc1de1aa6f8845
xan2301 <2silverghost2@gmail.com>
2022-10-20 23:02:11 +0200

    17. Initializing Data with Spring

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5webapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
@@ -21,16 +22,16 @@ public class Author {
 
 
     @ManyToMany(mappedBy = "authors")
-    private Set<Book> books;
+    private Set<Book> books = new HashSet<>();
 
 
     public Author() {
     }
 
-    public Author(String firstName, String lastname, Set<Book> books) {
+    public Author(String firstName, String lastname) {
         this.firstName = firstName;
         this.lastname = lastname;
-        this.books = books;
+
     }
 
     public Long getId() {
